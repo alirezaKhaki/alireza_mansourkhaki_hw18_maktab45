12827efaaf1e12f0c7bd8197854d77ce0266b72f
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-03-13 21:09:14 +0330

    dahboard page created and logout function added to dashboard route

--- tamrin/routes/dashboard.js
+++ tamrin/routes/dashboard.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const generalTools = require('../tools/general-tools');
 
 router.get('/', generalTools.loginChecker, (req, res) => {
-
+    const user = req.session.user
+    res.render('dashboard', { user })
 });
 
+router.get('/logout', (req, res) => {
+    console.log(req.cookies.user_sid);
+    res.clearCookie("user_sid");
+    res.redirect('/api/dashboard')
+
+})
 
 module.exports = router;
