09bf76421b3f43aa9451b9747350016940799535
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-03-15 13:11:33 +0330

    login page UI created

--- tamrin/public/javascripts/login.js
+++ tamrin/public/javascripts/login.js
@@ -1,10 +1,12 @@
 $(function() {
-
+    $('body').on('click', '#signup', function() {
+        window.location.href = '/api/register'
+    })
     $('body').on('click', '#register', function() {
         const username = $('#username').val()
         const password = $('#password').val()
         if (username.length === 0 || password.length === 0) {
-            return alert("please fill the inputs")
+            return $('.modal-body').html(''), $('.modal-body').html('please fill the inputs'), $("#triger").click();
         }
 
         const newUser = {
@@ -26,7 +28,9 @@ $(function() {
 
 
                 } else if (data.msg === 'incorrect username or password') {
-                    alert('incorrect username or password')
+                    $('.modal-body').html('')
+                    $('.modal-body').html('incorrect username or password')
+                    $("#triger").click();
                 }
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -39,3 +43,28 @@ $(function() {
         });
     })
 });
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    console.log(1);
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
